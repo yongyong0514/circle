48223215264f044ff9139625f5d0f9a4657784bb
YONG <yongyong0514@outlook.com>
2020-11-26 09:19:04 +0900

    11.26 주석 처리 및 오류 수정

--- Circle/src/main/java/com/kh/circle/post/contorller/PostController.java
+++ Circle/src/main/java/com/kh/circle/post/contorller/PostController.java
@@ -39,57 +39,6 @@ public class PostController {
 	@Autowired
 	private HttpServletResponse response;
 
-	@GetMapping("postList")
-	public String postList(Model model, PostPaging paging) {
-	
-		// pageing calculation
-		int currentPageNo = 1;
-		int recordsPerPage = 0;
-		String url = null;
-		
-		//커넥션 풀 연결 / 인스턴스 생성
-
-//		PostService postService = PostService.getInstance();
-
-		//pages, lines 파라미터를 받아 currnetPageNo, recordsPerpage대입
-		// 처음 페이지 열릴 때에는 당연히 1, 0
-		if(request.getParameter("pages") != null)
-			currentPageNo = Integer.parseInt(request.getParameter("pages"));
-		
-		if(request.getParameter("lines") != null)
-			recordsPerPage = Integer.parseInt(request.getParameter("lines"));
-		
-		
-		//Paging 객체 생성(currentPagenNO, recordsPerPage를 인자로 넣고 초기화함)
-		//객체 선언한 뒤 paging 출력하면 recordsPerPage가 5로 출력
-		PostPaging postPaging = new PostPaging(currentPageNo, recordsPerPage);
-		
-		
-		//해당 게시글의 인덱스를 구하는 변수offset
-		
-		int offset = (postPaging.getCurrentPageNo() - 1) * postPaging.getRecordsPerPage();
-		
-		
-		
-		//post 데이터 가져오기
-		List<Post> post = postService.getPostList(offset, postPaging.getRecordsPerPage());
-		
-		//전체 갯수 구해서 numberOfRecords 메소드 세팅
-		//*************** 이 인근 이해안감
-		postPaging.setNumberOfRecords(postService.getNoOfRecords());
-		
-		
-		//paging 생성
-		paging.makePaging();
-		
-		
-		//list 존재시
-		request.setAttribute("post", post);
-		request.setAttribute("paging", paging);
-		
-		return "post/postList";
-	}
-
 	/*
 	 * 페이징처리한 파트 실패함
 	 * 
