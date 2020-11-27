a56ada0a90cc47c81a396fac92b42ea276de21c1
hojong1 <ghwhd455@gmail.com>
2020-11-27 20:45:11 +0900

    20201127_baek_02 동호히게시글 작성/파일 첨부 구현중

--- Circle/src/main/java/com/kh/circle/comu/controller/ComuController.java
+++ Circle/src/main/java/com/kh/circle/comu/controller/ComuController.java
@@ -18,59 +18,64 @@ import com.kh.circle.comu.entity.Comu;
 import com.kh.circle.comu.entity.ComuPager;
 import com.kh.circle.comu.service.ComuService;
 
-
 @Controller
 @RequestMapping("/community")
 public class ComuController {
-	
+
 	@Autowired
 	private SqlSession sqlSession;
-	
+
 	@Autowired
 	private ComuService service;
-	
-	
-	//게시글 가져오기 
+
+	// 게시글 가져오기
 	@GetMapping("/comuList")
-	public ModelAndView comuList(@RequestParam(defaultValue="1")int curPage)throws Exception {
-		//페이징 처리
+	public ModelAndView comuList(@RequestParam(defaultValue = "1") int curPage) throws Exception {
+		// 페이징 처리
 		int count = service.comuListarticle();
-		ComuPager pager = new ComuPager(count,curPage);
+		ComuPager pager = new ComuPager(count, curPage);
 		int start = pager.getPageBegin();
 		int end = pager.getPageEnd();
-		//게시글 리스트
+		// 게시글 리스트
 		ModelAndView mav = new ModelAndView();
-		List<Comu> comuList = service.comuList(start,end/*페이징에 필요한거 넣어주기*/);
+		List<Comu> comuList = service.comuList(start, end/* 페이징에 필요한거 넣어주기 */);
 		mav.setViewName("community/comuList");
-		Map<String,Object> map = new HashMap<String,Object>();
-		map.put("list",comuList);
-		//맵에 pager 넣는다
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("list", comuList);
+		// 맵에 pager 넣는다
 		map.put("pager", pager);
-		mav.addObject("map",map);
-		//model.addAttribute("comuList",comuList);
-		for(Comu a: comuList) {
-			System.out.println("comuController 값 넘어오나?"+a.getComu_post_ordr()+a.getComu_post_cont());
+		mav.addObject("map", map);
+		// model.addAttribute("comuList",comuList);
+		for (Comu a : comuList) {
+			System.out.println("comuController 값 넘어오나?" + a.getComu_post_ordr() + a.getComu_post_cont());
 		}
-		
+
 		return mav;
 	}
-	
+	@RequestMapping(value="/comuAdd")
+	public String comuAdd() {
+		return "comuAdd";
+	}
+
 	@GetMapping("/comuInfoList")
 	public String comuInfoList() {
 		return "community/comuInfoList";
 	}
+
 	@GetMapping("/comuJoin")
 	public String comuJoin() {
 		return "community/comuJoin";
 	}
+
 	// 동호회 게시글 등록
 	@GetMapping("/comuAdd")
 	public String comuAdd(Model model, @ModelAttribute("comu.comuList") Comu comu) {
-		
-		model.addAttribute("comu",comu);
-		
+
+		model.addAttribute("comu", comu);
+
 		return "community/comuAdd";
 	}
+
 	@GetMapping("/comuApp")
 	public String comuApp() {
 		return "community/comuApp";
