195af344eee876eed74b033442022c9fb3712144
chaDongWun <lovewave02@naver.com>
2020-12-05 21:02:41 +0900

    진행중인 설문 페이지 조회 기능 생성

--- Circle/src/main/java/com/kh/circle/poll/repository/PollDaoImpl.java
+++ Circle/src/main/java/com/kh/circle/poll/repository/PollDaoImpl.java
@@ -7,6 +7,8 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.kh.circle.poll.entity.Pagination;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -17,9 +19,26 @@ public class PollDaoImpl implements PollDao{
 	SqlSession sqlSession;
 	
 	@Override
-	public List<HashMap<String, String>> homeList(String emp_no) {
-		List<HashMap<String, String>> list = sqlSession.selectList("poll.homeList", emp_no);
+	public List<HashMap<String, String>> homeList(String empNo) {
+		List<HashMap<String, String>> list = sqlSession.selectList("poll.homeList", empNo);
 		return list;
+	}
+
+	@Override
+	public List<HashMap<String, String>> getResult(HashMap<String, String> params) {
+		List<HashMap<String, String>> list = sqlSession.selectList("poll.getResult", params);
+		return list;
+	}
+
+	@Override
+	public List<HashMap<String, String>> progressList(Pagination pageInfo) {
+		List<HashMap<String, String>> list = sqlSession.selectList("poll.progressList", pageInfo);
+		return list;
+	}
+
+	@Override
+	public int countTotalProgressPost(Pagination totalPageInfo) {
+		return sqlSession.selectOne("poll.countTotalProgressPost", totalPageInfo);
 	}
 
 }
--- Circle/src/main/java/com/kh/circle/poll/service/PollServiceImpl.java
+++ Circle/src/main/java/com/kh/circle/poll/service/PollServiceImpl.java
@@ -6,8 +6,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.kh.circle.poll.entity.Pagination;
 import com.kh.circle.poll.repository.PollDao;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Service
 public class PollServiceImpl implements PollService{
 	
@@ -15,11 +19,30 @@ public class PollServiceImpl implements PollService{
 	private PollDao pollDao;
 	
 	@Override
-	public List<HashMap<String, String>> homeList(String emp_no) {
-		
-		List<HashMap<String, String>> list = pollDao.homeList(emp_no);
+	public List<HashMap<String, String>> homeList(String empNo) {
+		List<HashMap<String, String>> list = pollDao.homeList(empNo);
+		return list;
+	}
+
+	@Override
+	public List<HashMap<String, String>> getResult(HashMap<String, String> params) {
+		List<HashMap<String, String>> list = pollDao.getResult(params);
+		return list;
+	}
+
+	@Override
+	public List<HashMap<String, String>> progressList(Pagination pageInfo) {
+
+		List<HashMap<String, String>> list = pollDao.progressList(pageInfo);
 		
 		return list;
 	}
+
+	@Override
+	public int countTotalProgressPost(Pagination totalPageInfo) {
+		int total = pollDao.countTotalProgressPost(totalPageInfo);
+		return total;
+	}
+	
 
 }
