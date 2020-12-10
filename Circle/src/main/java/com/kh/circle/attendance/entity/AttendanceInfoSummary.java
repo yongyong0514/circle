a2644ae24d533d911b2893da5bd108241fa4c1c8
CHO <CHO@PC-CHO>
2020-12-11 05:01:08 +0900

    20201211 조병근
    . 수정 중...

--- Circle/src/main/java/com/kh/circle/attendance/entity/AttendanceInfoSummary.java
+++ Circle/src/main/java/com/kh/circle/attendance/entity/AttendanceInfoSummary.java
@@ -21,6 +21,9 @@ public class AttendanceInfoSummary {
 	private String over_worktime_week_hour;		//주간 초과근무(시간)
 	private String over_worktime_week_minute;	//주간 초과 근무(분)
 	
+	private String startDate;					//시작일
+	private String endDate;						//종료일
+	
 	private List<WorkAndOff> workAndOffList;	//근무시간 리스트
 	
 }
